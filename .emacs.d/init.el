13ca00f3768bff560e184ce8c6ee2869bc5c65f3
James Fu <fzjames2037@gmail.com>
2023-01-05 17:59:58 -0700

    Added Haskell mode and Yasnippet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -455,6 +455,7 @@
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
 (add-to-list 'org-structure-template-alist '("sa" . "src sage"))
 (add-to-list 'org-structure-template-alist '("e" . "example"))
+(add-to-list 'org-structure-template-alist '("ha" . "haskell"))
 
 (use-package org-roam
   :init
@@ -625,3 +626,21 @@
 (use-package eglot
   :hook ((c-mode . eglot-ensure)
          (c++-mode . eglot-ensure)))
+
+(use-package haskell-mode)
+
+(use-package yasnippet
+  :config (yas-global-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yasnippet which-key use-package restart-emacs rainbow-delimiters projectile ox-hugo org-superstar org-roam-ui org-drill org-bullets org-appear ob-sagemath magit key-chord ivy-rich helpful haskell-mode general flycheck evil-easymotion evil-collection elfeed-org eglot diminish deft counsel company auto-package-update)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
